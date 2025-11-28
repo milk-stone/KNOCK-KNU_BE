1b04707bfe3cdd7d15d2f6417ec521b4e3488d3c
milk-stone <ywshero@naver.com>
2025-11-29 02:02:49 +0900

    feat: Store(가게) 엔티티 DB 저장 컬럼명 지정

--- src/main/java/com/knock_knu/KNOCK_KNU_BE/domain/store/Store.java
+++ src/main/java/com/knock_knu/KNOCK_KNU_BE/domain/store/Store.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 public class Store {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_id")
     private Long id;
 
     @Column(nullable = false, length = 100)
