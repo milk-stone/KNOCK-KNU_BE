93b8eb78a47c8fe65d7d5e232602a52a6045ae76
milk-stone <ywshero@naver.com>
2025-11-29 22:41:15 +0900

    feat: Gemini API 이용, 운세 작업 간 필요한 ErrorCode 추가

--- src/main/java/com/knock_knu/KNOCK_KNU_BE/global/exception/ErrorCode.java
+++ src/main/java/com/knock_knu/KNOCK_KNU_BE/global/exception/ErrorCode.java
@@ -10,7 +10,12 @@ public enum ErrorCode {
     WRONG_DOOR_NAME(HttpStatus.BAD_REQUEST, "SE_001", "잘못된 문 이름입니다."),
     WRONG_MODIFIER_NAME(HttpStatus.BAD_REQUEST, "SE_002", "존재하지 않는 수식어입니다."),
     WRONG_PLACE_NAME(HttpStatus.BAD_REQUEST, "SE_003", "존재하지 않는 장소 카테고리입니다."),
-    STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "SE_004", "해당 ID의 가게가 존재하지 않습니다.");
+    STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "SE_004", "해당 ID의 가게가 존재하지 않습니다."),
+
+    FORTUNE_NOT_FOUND(HttpStatus.NOT_FOUND, "FE_001", "해당 운세가 존재하지 않습니다."),
+
+    GEMINI_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "GE_001", "Gemini API 호출 중 오류가 발생했습니다."),
+    FORTUNE_PARSING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "GE_002", "운세 데이터 변환에 실패했습니다.");
     private final HttpStatus status;
     private final String code;
     private final String message;
